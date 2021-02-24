b61f8e10689f8ec6842b8ced759c5eeaf990e73a
shekarrmurthy <44881154+shekarrmurthy@users.noreply.github.com>
2021-02-24 02:04:46 -0700

    Add files via upload

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,28 @@
 # TODO: Designate a cloud provider, region, and credentials
-
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "<>"
+  secret_key = "<>"
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+resource "aws_instance" "Udacity_T2" {
+  count         = "4"
+  ami           = "ami-0c6b1d09930fac512"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0a185f4184e96c66e"
+  tags = {
+    name = "Udacity T2"
+  }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+#resource "aws_instance" "Udacity_M4" {
+#  count         = "2"
+#  ami           = "ami-0c6b1d09930fac512"
+#  instance_type = "m4.large"
+#  subnet_id     = "subnet-0a185f4184e96c66e"
+#  tags = {
+#    name = "Udacity M4"
+#  }
+#}
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,11 @@
 # TODO: Define the variable for aws_region
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "lambda_function_name" {
+  type        = string
+  description = "The lambda function name"
+  default     = "greet_lambda"
+}
